ed65e5ba03972dc05844f523829bbac3343850d3
mtchoi <mtchoi@skku.edu>
2025-01-12 14:47:01 +0900

    실시간 제자리 출력

--- git_test.py
+++ git_test.py
@@ -1,8 +1,10 @@
 import time
 from datetime import datetime
+import sys
 
 start_time = time.time()
 
 while time.time() - start_time < 5:
-    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4])
+    print(f"실시간: {datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-4]}", end='\r')
+    sys.stdout.flush()
     time.sleep(0.1)
